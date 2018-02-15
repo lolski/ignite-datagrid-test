81f46e30d6ea3c9ff6e9bd18b076496663bfdf24
Ganeshwara Herawan Hananda <lolski@Ganeshwaras-MacBook-Pro.local>
2018-02-15 19:49:29 +0000

    WIP

--- src/main/java/com/lolski/ignite/IgniteCluster.java
+++ src/main/java/com/lolski/ignite/IgniteCluster.java
@@ -3,6 +3,7 @@ package com.lolski.ignite;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpi;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 
@@ -15,7 +16,19 @@ public class IgniteCluster {
 
     public IgniteCluster(String localAddress, String... seeds) {
 //        this.PORT = port;
-        this.igniteConfiguration = new IgniteConfiguration().setDiscoverySpi(getTcpDiscoverySpi(localAddress, seeds));
+        this.igniteConfiguration = new IgniteConfiguration()
+                .setCommunicationSpi(getTcpCommunicationSpi(localAddress))
+                .setDiscoverySpi(getTcpDiscoverySpi(seeds));
+
+// listen to localhost:5555
+Ignition.start(new IgniteConfiguration()
+    .setCommunicationSpi(new TcpCommunicationSpi().setLocalAddress("localhost").setLocalPort(5555))
+    .setDiscoverySpi(new TcpDiscoverySpi().setIpFinder(new TcpDiscoveryVmIpFinder().setShared(true).setAddresses(Arrays.asList()))));
+
+// listen to localhost:5556, form a cluster with localhost:5555
+Ignition.start(new IgniteConfiguration()
+    .setCommunicationSpi(new TcpCommunicationSpi().setLocalAddress("localhost").setLocalPort(5556))
+    .setDiscoverySpi(new TcpDiscoverySpi().setIpFinder(new TcpDiscoveryVmIpFinder().setShared(true).setAddresses(Arrays.asList("localhost:5555")))));
     }
 
     public IgniteCluster start() {
@@ -23,12 +36,20 @@ public class IgniteCluster {
         return this;
     }
 
-    private TcpDiscoverySpi getTcpDiscoverySpi(String localAddress, String... seeds) {
-        TcpDiscoverySpi spi = new TcpDiscoverySpi();
-        TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder();
-        ipFinder.setAddresses(Arrays.asList(seeds));
-        spi.setLocalAddress(localAddress);
-        spi.setIpFinder(ipFinder);
+    private TcpCommunicationSpi getTcpCommunicationSpi(String localAddress) {
+        String[] hostAndPort = localAddress.split(":");
+        return new TcpCommunicationSpi()
+                .setLocalAddress(hostAndPort[0])
+                .setLocalPort(Integer.parseInt(hostAndPort[1]));
+    }
+
+    private TcpDiscoverySpi getTcpDiscoverySpi(String... seeds) {
+        TcpDiscoveryVmIpFinder ipFinder =
+                new TcpDiscoveryVmIpFinder()
+                .setShared(true)
+                .setAddresses(Arrays.asList(seeds));
+        TcpDiscoverySpi spi = new TcpDiscoverySpi()
+            .setIpFinder(ipFinder);
         return spi;
     }
 }
--- src/main/java/com/lolski/ignite/Main.java
+++ src/main/java/com/lolski/ignite/Main.java
@@ -7,15 +7,13 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-//        assertArgumentCorrect(args);
-//
-//        // TODO: use host port
-//        String host = args[0];
-//        int port = Integer.parseInt(args[1]);
         String localAddress = getLocalNodeAndSeeds(args).getKey();
         String[] seeds = getLocalNodeAndSeeds(args).getValue().toArray(new String[] {});
 
         IgniteCluster cluster = new IgniteCluster(localAddress, seeds).start();
+        System.out.println("====");
+        cluster.ignite.cluster().localNode().addresses().forEach(System.out::println);
+        System.out.println("====");
 //        IgniteMultiMap map = new IgniteMultiMap("test").getOrCreate(cluster.ignite);
     }
 
@@ -27,21 +25,6 @@ public class Main {
                 return new HashMap.SimpleEntry<>(args[0], Arrays.asList());
             default:
                 return new HashMap.SimpleEntry<>(args[0], Arrays.asList(Arrays.copyOfRange(args, 1, args.length)));
-        }
-    }
-
-    private static void assertArgumentCorrect(String[] args) {
-        String help = "Usage: <program> <host> <port> (port must be a number)";
-        if (args.length != 2) {
-            System.err.println(help);
-            System.exit(1);
-        }
-
-        try {
-            Integer.parseInt(args[1]);
-        } catch (NumberFormatException e) {
-            System.err.println(help);
-            System.exit(1);
         }
     }
 }
